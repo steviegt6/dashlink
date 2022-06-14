bc326f240bd91715a288ca2f8b9608fbd4616401
Tomat <xxlennygamerxx@gmail.com>
2022-06-14 10:09:52 -0700

    Simple byte reading.

--- TestDashlink.hx
+++ TestDashlink.hx
@@ -1,9 +1,13 @@
+import dashlink.Test;
+import dashlink.Utils;
 import dashlink.Op;
-import hl.Type;
 
 class TestDashlink {
 	public static function main():Void {
 		trace("Hello, world!");
-        trace("OMov arguments: " + Type.enumParameters(OMov(0, 0)));
+        trace("OMov nargs: " + Utils.OpNArgs[OMov]);
+
+        var bytes = Test.getFileBytes("hello.hl");
+        trace("Test program bytes:\n" + bytes);
 	}
 }
--- dashlink/Test.hx
+++ dashlink/Test.hx
@@ -1,4 +1,10 @@
 package dashlink;
 
+import sys.io.File;
+import haxe.io.Bytes;
+
 class Test {
+    public static function getFileBytes(path:String): Bytes {
+        return File.getBytes(path);
+    }
 }
