4e6d7c8c1b41cf507343abae7026137841c42959
Tomat <xxlennygamerxx@gmail.com>
2022-06-14 07:36:29 -0700

    Push the initial HL project.

---
+++ TestDashlink.hx
@@ -0,0 +1,9 @@
+import dashlink.Op;
+import hl.Type;
+
+class TestDashlink {
+	public static function main():Void {
+		trace("Hello, world!");
+        trace("OMov arguments: " + Type.enumParameters(OMov(0, 0)));
+	}
+}
---
+++ dashlink/Op.hx
@@ -0,0 +1,9 @@
+package dashlink;
+
+/**
+ * Represents HashLink opcodes.
+ */
+enum Op {
+    // TODO: Could just specify p count like in HashLink?
+	OMov(p1:Int, p2:Int);
+}
