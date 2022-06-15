d88010332c0bf1818e6d8cdffffce4fe2083218d
Tomat <xxlennygamerxx@gmail.com>
2022-06-14 23:41:33 -0700

    guh

---
+++ dashlink/api/IHlReader.hx
@@ -0,0 +1,24 @@
+// Adapted from https://github.com/Gui-Yom/hlbc/blob/master/src/deser.rs under the MIT license.
+
+package dashlink.api;
+
+import hl.UI8;
+
+// TODO: Make missing types.
+
+/**
+ * Core functions for deserializing a HashLink file.
+ */
+interface IHlReader {
+	function readSignedVariable():Int;
+	function readUnsignedVariable():UInt;
+	function readStrings(nstrings:UInt):Array<String>;
+	function readField():ObjField;
+	function readRefType():RefType;
+	function readTypeFun():TypeFun;
+	function readTypeObj():TypeObj;
+	function readType():Type;
+	function readNative():Native;
+	function readFunction(hasDebug:Bool, version:UI8):Function;
+	function readConstantDef():ConstantDef;
+}
---
+++ dashlink/impl/HlReader.hx
@@ -0,0 +1,10 @@
+package dashlink.impl;
+
+import dashlink.api.IHlReader;
+
+/**
+ * Standard implementation of `dashlink.api.IHlReader`.
+ */
+class HlReader implements IHlReader {
+
+}
